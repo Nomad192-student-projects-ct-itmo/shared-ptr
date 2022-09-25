67c4e85d504ae4f68518932397b1a6828707dcaa
Nomad192 <verenya08@gmail.com>
2022-09-25 23:36:15 +0300

    edits

--- shared-ptr.h
+++ shared-ptr.h
@@ -1,47 +1,45 @@
 #pragma once
 
 #include <cstddef>
-#include <iostream>
 #include <utility>
+#include <algorithm>
 
-// template <typename T> // нельзя из-за алиасинга
-struct control_block // without 2 alloc
+// template <typename T>  /// it is impossible because of aliasing
+struct control_block
 {
-  // unsigned long long
-  size_t strong_ref = 0;
-  size_t weak_ref = 0;
+  unsigned long strong_ref = 0;
+  unsigned long weak_ref = 0;
 
   virtual ~control_block() = default;
   virtual void unlink() = 0;
-  // virtual void get_ptr() = 0; //не жалко добавить ещё, вместо T *view
-  // //нельзя из-за алиасинга
+  // virtual T* get_ptr() = 0; ///it is impossible because of aliasing
 };
 
 template <typename T>
 struct ptr_block : control_block {
-  ptr_block(T* p) : p(p) {}
-
   T* p;
 
-  void unlink() {
+  explicit ptr_block(T* p) : p(p) {}
+
+  void unlink() override {
     delete p;
-    p = nullptr; //можно не писать
+    p = nullptr; //you don't have to write
   }
 
-  ~ptr_block() {
-    unlink();
-  }
+//  ~ptr_block() {
+//    unlink();
+//  }
 };
 
 template <typename T>
 struct obj_block : control_block {
-  // T o; // bad, double delete
-  // aligned_storage <5, 2>; // занимает ровно столько же места, читабельней
-  // std optional; // больше места занимает чем надо
-  alignas(alignof(T)) char o[sizeof(T)]; // менее читабельней
+  // T o;                                             /// bad, double delete
+  // std optional;                                    /// takes up more space than necessary
+  //alignas(alignof(T)) char o[sizeof(T)]{};          /// less readable, alignas(alignof(T)) - alignment
+  std::aligned_storage <sizeof(T), alignof(T)> o[1];     /// takes up exactly the same amount of space, more readable
 
   template <typename... Args>
-  obj_block(Args&&... args) {
+  explicit obj_block(Args&&... args) {
     new (o) T(std::forward<Args>(args)...);
   }
 
@@ -49,7 +47,7 @@ struct obj_block : control_block {
     return reinterpret_cast<T*>(o);
   }
 
-  void unlink() {
+  void unlink() override {
     reinterpret_cast<T*>(o)->~T();
   }
 };
@@ -59,26 +57,36 @@ class weak_ptr;
 
 template <typename T>
 class shared_ptr {
-public:
-  shared_ptr() noexcept;
-  shared_ptr(nullptr_t) noexcept;
-  shared_ptr(T* ptr_);
+  control_block* cb = nullptr;
+  T* obj = nullptr;
 
-  shared_ptr(const shared_ptr& other) noexcept;
-  shared_ptr(shared_ptr&& other) noexcept;
+public:
+  shared_ptr() noexcept = default;
+  explicit shared_ptr(std::nullptr_t) noexcept : cb(new ptr_block<T>(nullptr)) {}
+  explicit shared_ptr(T* ptr) {make_cb(ptr);}
+
+  shared_ptr(const shared_ptr& other) noexcept : cb(other.cb), obj(other.obj) {
+    if (cb)
+      cb->strong_ref++;
+  }
+  shared_ptr(shared_ptr&& other) noexcept : cb(other.cb), obj(other.obj) {
+    other.obj = nullptr;
+    other.cb = nullptr;
+  }
   shared_ptr<T>& operator=(const shared_ptr& other) noexcept;
   shared_ptr<T>& operator=(shared_ptr&& other) noexcept;
 
-  T* get() const noexcept;
-  operator bool() const noexcept;
-  T& operator*() const noexcept;
-  T* operator->() const noexcept;
+  T* get() const noexcept         {return obj;}
+  operator bool() const noexcept  {return obj != nullptr;}
+  T& operator*() const noexcept   {return *obj;}
+  T* operator->() const noexcept  {return obj;}
 
-  std::size_t use_count() const noexcept;
-  void reset() noexcept;
-  void reset(T* new_ptr);
+  std::size_t use_count() const noexcept {return cb ? cb->strong_ref : 0;}
+  void reset() noexcept   {unlink();}
+  void reset(T* new_ptr)  {unlink(); make_cb(new_ptr);}
 
-  ~shared_ptr() noexcept;
+  void unlink() noexcept;
+  ~shared_ptr() noexcept  {unlink();}
 
 private:
   template <typename msT, typename... Args>
@@ -87,21 +95,22 @@ private:
   friend class weak_ptr<T>;
 
   shared_ptr(control_block* cb, T* ptr) : cb(cb), obj(ptr) {}
-  control_block* cb;
-  T* obj;
 
-  shared_ptr<T>& sp_copy(const shared_ptr& other) noexcept;
-  shared_ptr<T>& sp_move(shared_ptr&& other) noexcept;
+  //shared_ptr<T>& sp_copy(const shared_ptr& other) noexcept;
+  //shared_ptr<T>& sp_move(shared_ptr&& other) noexcept;
   void make_cb(T* ptr);
+  void swap(shared_ptr<T> &sp) noexcept
+  {
+    std::swap(cb, sp.cb);
+    std::swap(obj, sp.obj);
+  }
 };
 
 template <typename T>
 class weak_ptr {
 public:
-  weak_ptr() noexcept {}
-  weak_ptr(const shared_ptr<T>& other) noexcept {
-    cb = other.cb;
-    obj = other.obj;
+  weak_ptr() noexcept = default;
+  weak_ptr(const shared_ptr<T>& other) noexcept : cb(other.cb), obj(other.obj) {
     if (other.cb)
       cb->weak_ref++;
   }
@@ -136,22 +145,16 @@ private:
 
 template <typename T, typename... Args>
 shared_ptr<T> make_shared(Args&&... args) {
-  obj_block<T>* ob = new obj_block<T>(std::forward<Args>(args)...);
+  auto* ob = new obj_block<T>(std::forward<Args>(args)...);
   ob->strong_ref++;
   return shared_ptr(ob, ob->get());
 }
 
-template <typename T>
-shared_ptr<T>::shared_ptr() noexcept {
-  cb = nullptr;
-  obj = nullptr;
-}
-
-template <typename T>
-shared_ptr<T>::shared_ptr(nullptr_t) noexcept {
-  cb = nullptr; // cb
-  obj = nullptr;
-}
+//template <typename T>
+//shared_ptr<T>::shared_ptr() noexcept {
+//  cb = nullptr;
+//  obj = nullptr;
+//}
 
 template <typename T>
 void shared_ptr<T>::make_cb(T* ptr) {
@@ -165,44 +168,30 @@ void shared_ptr<T>::make_cb(T* ptr) {
   cb->strong_ref++;
 }
 
-template <typename T>
-shared_ptr<T>::shared_ptr(T* ptr) {
-  make_cb(ptr);
-}
+//template <typename T>
+//shared_ptr<T>& shared_ptr<T>::sp_copy(const shared_ptr& other) noexcept {
+//  obj = other.obj;
+//  cb = other.cb;
+//  if (cb)
+//    cb->strong_ref++;
+//
+//  return *this;
+//}
+//
+//template <typename T>
+//shared_ptr<T>& shared_ptr<T>::sp_move(shared_ptr&& other) noexcept {
+//  obj = other.obj;
+//  other.obj = nullptr;
+//
+//  cb = other.cb;
+//  other.cb = nullptr;
+//
+//  return *this;
+//}
 
 template <typename T>
-shared_ptr<T>& shared_ptr<T>::sp_copy(const shared_ptr& other) noexcept {
-  obj = other.obj;
-  cb = other.cb;
-  if (cb)
-    cb->strong_ref++;
-
-  return *this;
-}
-
-template <typename T>
-shared_ptr<T>& shared_ptr<T>::sp_move(shared_ptr&& other) noexcept {
-  obj = other.obj;
-  other.obj = nullptr;
-
-  cb = other.cb;
-  other.cb = nullptr;
-
-  return *this;
-}
-
-template <typename T>
-shared_ptr<T>::shared_ptr(const shared_ptr& other) noexcept {
-  sp_copy(other);
-}
-
-template <typename T>
-shared_ptr<T>::shared_ptr(shared_ptr&& other) noexcept {
-  sp_move(std::move(other));
-}
-
-template <typename T>
-shared_ptr<T>::~shared_ptr() noexcept {
+void shared_ptr<T>::unlink() noexcept
+{
   if (cb) {
     cb->strong_ref--;
     if (cb->strong_ref == 0)
@@ -216,54 +205,14 @@ shared_ptr<T>::~shared_ptr() noexcept {
 
 template <typename T>
 shared_ptr<T>& shared_ptr<T>::operator=(const shared_ptr& other) noexcept {
-  if (this != &other) {
-    this->~shared_ptr();
-    return sp_copy(other);
-  } else
-    return *this;
+  shared_ptr<T> new_sp(other);
+  this->swap(new_sp);
+  return *this;
 }
 
 template <typename T>
 shared_ptr<T>& shared_ptr<T>::operator=(shared_ptr&& other) noexcept {
-  if (this != &other) {
-    this->~shared_ptr();
-    return sp_move(std::move(other));
-  } else
-    return *this;
-}
-
-template <typename T>
-T* shared_ptr<T>::get() const noexcept {
-  return obj;
-}
-
-template <typename T>
-shared_ptr<T>::operator bool() const noexcept {
-  return obj != nullptr;
-}
-
-template <typename T>
-T& shared_ptr<T>::operator*() const noexcept {
-  return *obj;
-}
-
-template <typename T>
-T* shared_ptr<T>::operator->() const noexcept {
-  return obj;
-}
-
-template <typename T>
-std::size_t shared_ptr<T>::use_count() const noexcept {
-  return cb ? cb->strong_ref : 0;
-}
-
-template <typename T>
-void shared_ptr<T>::reset() noexcept {
-  this->~shared_ptr();
-}
-
-template <typename T>
-void shared_ptr<T>::reset(T* new_ptr) {
-  this->~shared_ptr();
-  make_cb(new_ptr);
+  shared_ptr<T> new_sp(std::move(other));
+  this->swap(new_sp);
+  return *this;
 }
